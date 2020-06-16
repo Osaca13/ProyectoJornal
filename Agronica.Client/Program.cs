27dcde1f60115d2e0b534abb769321fc79ec2e08
O SaCa <odlyss@gmail.com>
2020-06-17 00:16:15 +0200

    Agregar archivos de proyecto.

---
+++ Agronica.Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using Agronica.Client.Services.Contracts;
+using Agronica.Client.Services.Implementations;
+using Agronica.Client.States;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Agronica.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddOptions();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<IdentityAuthenticationStateProvider>();
+            builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<IdentityAuthenticationStateProvider>());
+            builder.Services.AddScoped<IAuthorizeApi, AuthorizeApi>();
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+            var host = builder.Build();
+            await host.RunAsync();
+        }
+    }
+}
---
+++ Agronica.Shared/RegisterParameters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Agronica.Shared
+{
+    public class RegisterParameters
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password), ErrorMessage = "Passwords do not match")]
+        public string PasswordConfirm { get; set; }
+
+        [Required]
+        public string Correo { get; set; }
+
+        [Required]
+        [Compare(nameof(Correo), ErrorMessage = "Correo no coincide")]
+        public bool CorreoConfirm { get; set; }
+    }
+}
